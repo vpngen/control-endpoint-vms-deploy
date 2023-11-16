6aba75c570a8acebb408be728fd047c65e1e76b9
niktheworker <niktheworker@gmail.com>
2023-11-16 19:21:05 +0400

    test_deploy.py: added def for exec command

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -1,10 +1,10 @@
 import pytest
 import paramiko
 
+# VMs IP addresses and SSH credentials
 vm_ct = '10.255.0.4'
 vm_ep = '10.255.0.5'
-hosts = [vm_ct, vm_ep]
-port = 22  # Default SSH port
+port = 22
 username = 'ubuntu'
 private_key_path = '/root/.ssh/id_rsa'
 
@@ -12,30 +12,32 @@ private_key_path = '/root/.ssh/id_rsa'
 ct_services_to_check = ["socat-apt-proxy", "socat-rsyslog-proxy", "socat-zabbix-proxy"]
 ep_services_to_check = ["zabbix-agent", "outline-ss-logger", "wg-mng.socket"]
 
-# Specify the private key for authentication
-private_key = paramiko.RSAKey(filename=private_key_path)
+#Function to execute remote commands on VMs
+def execute_remote_command(ssh, command):
+    stdin, stdout, stderr = ssh.exec_command(command)
+    return stdout.read().decode('utf-8').strip()
 
-for host in hosts:
-    def test_service():
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(
-            host,
-            port,
-            username,
-            private_key_path
-        )
-        if host == vm_ct:
-            for service in ct_services_to_check:
-                command = f'systemctl is-active {service}'
-                stdin, stdout, stderr = ssh.exec_command(command)
-                status = stdout.read().decode('utf-8').strip()
-                assert status == 'active'
-        else:
-            for service in ep_services_to_check:
-                command = f'systemctl is-active {service}'
-                stdin, stdout, stderr = ssh.exec_command(command)
-                status = stdout.read().decode('utf-8').strip()
-                assert status == 'active'
+# Test function to check services on VMs after deployment is finished
+def test_deploy():
+        for host in [vm_ct, vm_ep]:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(
+                host,
+                port,
+                username,
+                private_key_path
+            )
+
+            if host == vm_ct:
+                for service in ct_services_to_check:
+                    command = f'systemctl is-active {service}'
+                    status = execute_remote_command(ssh, command)
+                    assert status == 'active'
+            else:
+                for service in ep_services_to_check:
+                    command = f'systemctl is-active {service}'
+                    status = execute_remote_command(ssh, command)
+                    assert status == 'active'
 
         ssh.close()
