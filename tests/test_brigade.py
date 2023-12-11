49716918eb2b4ca00699bdcbe5abc07ab268186a
niktheworker <niktheworker@gmail.com>
2023-12-11 22:08:43 +0400

    bugfix test_brigade.py

--- tests/test_brigade.py
+++ tests/test_brigade.py
@@ -47,15 +47,15 @@ ep_services_to_check = [ep_services_to_check_all,
                         ep_services_to_check_outline]
 
 #List of curl requests add different VPNs to EPs
-curl_request_all = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_add='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`'&internal-nets=172.16.0.1/16,fd0d:86fa:c3bc::1/64&external-ip=195.133.0.107&l2tp-preshared-key='`echo 0123456789012345 | base64 -w 0 | nacl -b seal /etc/vg-router.json`'&wireguard-port=40000&cloak-domain=google.com&openvpn-ca-key='`cat pki/private/ca.key | gzip -n | base64 -w 0 | nacl -b seal /etc/vg-router.json`'&openvpn-ca-crt='`cat pki/ca.crt | gzip -n | base64 -w 0`'&outline-ss-port=9944'"""
+curl_request_all = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_add='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`'&internal-nets=172.16.0.1/16,fd0d:86fa:c3bc::1/64&external-ip=195.133.0.108&l2tp-preshared-key='`echo 0123456789012345 | base64 -w 0 | nacl -b seal /etc/vg-router.json`'&wireguard-port=40000&cloak-domain=google.com&openvpn-ca-key='`cat pki/private/ca.key | gzip -n | base64 -w 0 | nacl -b seal /etc/vg-router.json`'&openvpn-ca-crt='`cat pki/ca.crt | gzip -n | base64 -w 0`'&outline-ss-port=9944'"""
 
-curl_request_wg = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_add='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`'&internal-nets=172.16.0.1/16,fd0d:86fa:c3bc::1/64&external-ip=195.133.0.107&wireguard-port=40000'"""
+curl_request_wg = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_add='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`'&internal-nets=172.16.0.1/16,fd0d:86fa:c3bc::1/64&external-ip=195.133.0.108&wireguard-port=40000'"""
 
-curl_request_ipsec = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_add='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`'&internal-nets=172.16.0.1/16,fd0d:86fa:c3bc::1/64&external-ip=195.133.0.107&wireguard-port=40000&l2tp-preshared-key='`echo 0123456789012345 | base64 -w 0 | nacl -b seal /etc/vg-router.json`''"""
+curl_request_ipsec = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_add='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`'&internal-nets=172.16.0.1/16,fd0d:86fa:c3bc::1/64&external-ip=195.133.0.108&wireguard-port=40000&l2tp-preshared-key='`echo 0123456789012345 | base64 -w 0 | nacl -b seal /etc/vg-router.json`''"""
 
-curl_request_openvpn = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_add='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`'&internal-nets=172.16.0.1/16,fd0d:86fa:c3bc::1/64&external-ip=195.133.0.107&wireguard-port=40000&cloak-domain=google.com&openvpn-ca-key='`cat /home/ubuntu/pki/private/ca.key | gzip -n | base64 -w 0 | nacl -b seal /etc/vg-router.json`'&openvpn-ca-crt='`cat /home/ubuntu/pki/ca.crt | gzip -n | base64 -w 0`''"""
+curl_request_openvpn = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_add='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`'&internal-nets=172.16.0.1/16,fd0d:86fa:c3bc::1/64&external-ip=195.133.0.108&wireguard-port=40000&cloak-domain=google.com&openvpn-ca-key='`cat /home/ubuntu/pki/private/ca.key | gzip -n | base64 -w 0 | nacl -b seal /etc/vg-router.json`'&openvpn-ca-crt='`cat /home/ubuntu/pki/ca.crt | gzip -n | base64 -w 0`''"""
 
-curl_request_outline = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_add='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`'&internal-nets=172.16.0.1/16,fd0d:86fa:c3bc::1/64&external-ip=195.133.0.107&wireguard-port=40000&outline-ss-port=9944'"""
+curl_request_outline = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_add='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`'&internal-nets=172.16.0.1/16,fd0d:86fa:c3bc::1/64&external-ip=195.133.0.108&wireguard-port=40000&outline-ss-port=9944'"""
 
 curl_request = [curl_request_all,
                 curl_request_wg,
@@ -99,5 +99,7 @@ def test_main():
                     command = f'systemctl is-active {service}'
                     status = execute_remote_command(ssh, command)
                     assert status == 'active'
-
+            command_del = """curl -v 'http://[fdcc:c385:6c::3]:8080/?wg_del='`echo In4ningHYWutaNHXfkE79I3BF20oKzoWiizL7l2oOSM= | nacl -b seal /etc/vg-router.json`"""
+            response_del = execute_remote_command(ssh, command_del)
+            assert response_del == '{"code": "0"}' or response_del == '{"code": "128", "error": "no interface found for supplied private key"}'
             ssh.close()
